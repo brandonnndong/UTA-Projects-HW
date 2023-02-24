eedd0aa85819b0159d6c458ed2f23cfb87828daf
brandonnndong <brandonnndong@gmail.com>
2023-02-24 01:06:33 -0800

    P05 Sprint 1

---
+++ P05/Customer.java
@@ -0,0 +1,24 @@
+public class Customer extends Store{
+    public Customer(String name, String email){
+        super(name);
+        this.email = email;
+        String char1 = new String("@");
+        String char2 = new String(".");
+        int checkEmail1 = email.indexOf(char1);
+        int checkEmail2 = email.indexOf(char2);
+        if(checkEmail1 == -1 && checkEmail2 == -1){
+            throw new IllegalArgumentException("Invalid email");
+        }
+    }
+
+    public String toString(){
+        return name + " (" + email + ")";
+    }
+
+    public boolean equals(Object o){
+        return name == email;
+    }
+
+    private String name;
+    private String email;
+}
